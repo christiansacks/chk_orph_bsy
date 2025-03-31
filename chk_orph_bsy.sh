b0f5c8ead327b051e5b7e4610a89655e95320196
MeaTLoTioN <ml@erb.pw>
2025-03-31 18:18:40 +0100

    update script

--- chk_orph_bsy.sh
+++ chk_orph_bsy.sh
@@ -2,9 +2,9 @@
 
 WDIR="/mystic"; cd $WDIR
 
-F=$(ps -ef|grep "mis poll"|grep -v grep >/dev/null; [ $? -gt 0 ] && echo "NOT RUNNING" || echo "RUNNING")
+F=$(ps -ef|grep "[m]is poll" >/dev/null; [ $? -gt 0 ] && echo "NOT RUNNING" || echo "RUNNING")
 M=$(pidof mutil; [ $? -gt 0 ] && echo "NOT RUNNING" || echo "RUNNING")
-B=$(find . -type f|grep .bsy|grep echomail|wc -l)
+B=$(find -iname '*.bsy'|grep -c echomail)
 
 cat << EOF
 Fidopoll pidof result: $F
@@ -14,7 +14,7 @@ EOF
 
 if [[ $B -gt 0 && ($F == "NOT RUNNING" && $M == "NOT RUNNING") ]]; then
   echo -e "\nClearing $B orphaned BSY files..."
-  for F in $(find . -type f|grep .bsy|grep echomail); do
+  for F in $(find -iname '*.bsy'|grep echomail); do
     echo "Removing: $F"; rm $F
   done
 fi
